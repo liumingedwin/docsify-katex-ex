5068ba9e195f4a4731031755c291e1eb85e2a665
liumingedwin <liumingedwin@163.com>
2024-09-22 16:06:27 +0800

    REFACTOR: EASIER TO USE AND LESS BUGS

---
+++ README.md
@@ -0,0 +1,11 @@
+# Docsify KaTeX Handler Ex
+
+As you know, <https://github.com/upupming/docsify-katex.git> hasn't been developed for a long time. This one is a simple replacement. 
+
+## Usage
+```html
+<link rel="stylesheet" href="//gcore.jsdelivr.net/npm/katex/dist/katex.min.css">
+<script src="//gcore.jsdelivr.net/npm/katex/dist/katex.min.js"></script>
+<script src="//gcore.jsdelivr.net/npm/docsify-katex-ex"></script>
+```
+**For compatibility of older browser builds, use `<script src="//gcore.jsdelivr.net/npm/docsify-katex-ex/docsify-katex-ex.minimal.js"></script>` instead.** 
--- index.js
+++ index.js
@@ -1,29 +1,67 @@
-const replaceAll = String.prototype.replaceAll ? 
-(x, y, z) => (String.prototype.replaceAll.call(x, y, z)) 
-: require("string.prototype.replaceall");
-(( () => {
-function htmldecode(text){
-    let temp = document.createElement("div");
-    temp.innerHTML = text;
-    let output = temp.innerText || temp.textContent;
-    temp = null;
-    return output;
-}
-const BLOCK_REG = /\$\$[\s\S]*\$\$/g;
-const INLINE_REG = /\$[\s\S]*\$/g;
-  let docsify_katex_ex = (hook, vm) => {
-    hook.beforeEach((markdown) => {
-        let result_a  = (replaceAll(markdown, BLOCK_REG, (v) => {
-          return katex.renderToString(v.substr(2, v.length - 4), { displayMode: true });
-        }));
-        let final_result  = (replaceAll(result_a, INLINE_REG, (v) => {
-          v = htmldecode(v); // temp solution
-          return katex.renderToString(v.substr(1, v.length - 2), { displayMode: false });
-        }));
-      return final_result;
-    });
-  };
-  // Add plugin to docsify's plugin array
-  $docsify = $docsify || {};
-  $docsify.plugins = [].concat(docsify_katex_ex, $docsify.plugins || []);
+
+((() => {
+    $docsify = $docsify || {};
+    let htmldecode = (text) => {
+        let temp = document.createElement("div");
+        temp.innerHTML = text;
+        let output = temp.innerText || temp.textContent;
+        temp = null;
+        return output;
+    }
+    const renderer = katex.renderToString;
+    const block_opt = { displayMode: true, ...$docsify.docsify_katex_ex };
+    const inline_opt = { displayMode: false, ...$docsify.docsify_katex_ex };
+    const placeholder = new Date().toString();
+    let results_array = [];
+    let handle_inline = (markdown) => {
+        let pieces = markdown.split("$");
+        if (pieces.length % 2 == 0) {
+            console.warn("[Inline]Odd Pieces Length Found - %d", pieces.length);
+            return markdown;
+        }
+        let result = "";
+        for (let i in pieces) {
+            if (i & 1) {
+                results_array.push(renderer(pieces[i], inline_opt));
+                result += `${placeholder}${results_array.length - 1}${placeholder}`;
+            }
+            else {
+                result += pieces[i];
+            }
+        }
+        return result;
+    }
+    let handle_blocks = (markdown) => {
+        let pieces = markdown.split("$$");
+        if (pieces.length % 2 == 0) {
+            console.warn("[Blocks]Odd Pieces Length Found - %d", pieces.length);
+            return markdown;
+        }
+        let result = "";
+        for (let i in pieces) {
+            if (i & 1) {
+                results_array.push(renderer(htmldecode(pieces[i]), block_opt));
+                result += `${placeholder}${results_array.length - 1}${placeholder}`;
+            }
+            else {
+                result += handle_inline(pieces[i]);
+            }
+        }
+        return result;
+    }
+    let docsify_katex_ex = (hook, vm) => {
+        hook.beforeEach((markdown) => {
+            return handle_blocks(markdown);
+        });
+        hook.afterEach(function (html) {
+            for (let i in results_array) {
+                html = html.replace(`${placeholder}${i}${placeholder}`, results_array[i])
+            }
+            // ...
+            return html;
+        });
+
+    };
+    // Add plugin to docsify's plugin array
+    $docsify.plugins = [].concat(docsify_katex_ex, $docsify.plugins || []);
 })());
